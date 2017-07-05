708592bb16b520ff4a91d97b9d1478415d9cc0e7
nishant16 <nishant.rmps@gmail.com>
2017-07-05 22:00:31 +0530

    new1 commit

--- name.py
+++ name.py
@@ -8,4 +8,18 @@ num=input("enter the no." )
 a=fact(num)
 
 
+class Box:
+    def area(self):
+        return self.width*self.height
+
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+
+#Create an instance of Box
+x= Box(10,2)
+
+#print area
+print(x.area())
+
 
---
+++ name.py~
@@ -0,0 +1,11 @@
+def fact(num):
+    f=1
+    for i in range(1,num+1):
+        f=f*i
+    print f
+
+num=input("enter the no." )
+a=fact(num)
+
+
+
